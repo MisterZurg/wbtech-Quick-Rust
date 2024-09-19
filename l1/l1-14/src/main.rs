2f8fb7b4703af47e78292a2d173085403ecdc683
MisterZurg <misterzurg@gmail.com>
2024-09-19 22:34:17 +0300

    add l1.14

--- l1/l1-14/src/main.rs
+++ l1/l1-14/src/main.rs
@@ -1,5 +1,20 @@
-use std::any::*;
+use std::any::{Any, TypeId};
+
+fn get_type_at_runtime<T: Any>(_var: &T) {
+    if _var.type_id() == TypeId::of::<String>() {
+        println!("String")
+    } else if _var.type_id() == TypeId::of::<u128>() {
+        println!("u128")
+        // ...
+    } else {
+        println!("I donno")
+    }
+}
+
+struct Zeliboba();
 
 fn main() {
-    println!("Hello, world!");
+    get_type_at_runtime(&1_u128);
+    get_type_at_runtime(&"mgrachev".to_string());
+    get_type_at_runtime(&Zeliboba);
 }
