b7ea6906c7f23ab06adfb5484e7a00a03965a4b7
MisterZurg <misterzurg@gmail.com>
2024-09-19 23:28:05 +0300

    add l1.16 binary_search

--- l1/l1-16/src/main.rs
+++ l1/l1-16/src/main.rs
@@ -1,3 +1,32 @@
+use std::cmp::Ordering;
+
+fn binary_search(nums: &[i32], target: i32) -> i32 {
+    let mut l: usize = 0;
+    let mut r: usize = nums.len() - 1;
+
+    while l < r {
+        let m = (l + r) / 2;
+        match nums[m].cmp(&target) {
+            Ordering::Equal => return m as i32,
+            Ordering::Less => {
+                l = m + 1;
+            }
+            Ordering::Greater => {
+                r = m;
+            }
+        }
+    }
+
+    -1
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut nums = [1, 228, 13, 42, 64, 3, 6, 70, 1488];
+    println!("Raw: {:?}", nums);
+    nums.sort();
+    println!("Sorted: {:?}", nums);
+
+    println!("got 228 at idx {}", binary_search(&nums, 228));
+
+    println!("non exist number {}", binary_search(&nums, 4269));
 }
