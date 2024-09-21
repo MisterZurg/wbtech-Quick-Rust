eab436e8682a8d84f0284f01f566f4710614b832
MisterZurg <misterzurg@gmail.com>
2024-09-21 17:17:54 +0300

    add l1.18

---
+++ l1/l1-18/src/lib.rs
@@ -0,0 +1,4 @@
+/// reverse_line — helper function that reverses inputed string
+pub fn reverse_line(input: String) -> String {
+    input.chars().rev().collect::<String>()
+}
--- l1/l1-18/src/main.rs
+++ l1/l1-18/src/main.rs
@@ -1,3 +1,18 @@
+use l1_18::reverse_line;
+
+fn scan_input() -> String{
+    let mut line = String::new();
+    println!("Input line to reverse >");
+
+    std::io::stdin()
+        .read_line(&mut line)
+        .unwrap();
+
+    line
+}
+
 fn main() {
-    println!("Hello, world!");
+    let line = scan_input();
+
+    println!("{}", reverse_line(line));
 }
