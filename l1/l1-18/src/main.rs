16c283660c4ea2d12fae20cdc392fd3cf0cf2fde
MisterZurg <misterzurg@gmail.com>
2024-09-21 17:18:46 +0300

    add l1.18 test

--- l1/l1-18/src/main.rs
+++ l1/l1-18/src/main.rs
@@ -1,12 +1,10 @@
 use l1_18::reverse_line;
 
-fn scan_input() -> String{
+fn scan_input() -> String {
     let mut line = String::new();
     println!("Input line to reverse >");
 
-    std::io::stdin()
-        .read_line(&mut line)
-        .unwrap();
+    std::io::stdin().read_line(&mut line).unwrap();
 
     line
 }
