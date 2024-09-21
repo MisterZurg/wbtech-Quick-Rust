72a6a55d1301d06ded992fa169ea79bfd9a047d4
MisterZurg <misterzurg@gmail.com>
2024-09-21 16:58:48 +0300

    add l1.17

--- l1/l1-17/src/main.rs
+++ l1/l1-17/src/main.rs
@@ -1,3 +1,46 @@
+use std::sync::{Arc, Mutex, MutexGuard};
+use std::thread;
+
+const WORKERS: i32 = 10;
+
+/// Counter - ... —
+struct Counter {
+    calls: Mutex<i32>,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Counter {
+            calls: Mutex::from(0),
+        }
+    }
+
+    fn increment(&self) {
+        *(self.calls.lock().unwrap()) += 1;
+    }
+
+    fn get(&self) -> MutexGuard<'_, i32> {
+        self.calls.lock().unwrap()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cnt = Arc::new(Counter::new());
+    let mut thread_handles = Vec::new();
+
+    for _ in 0..WORKERS {
+        let cnt_clone = Arc::clone(&cnt);
+
+        let handle = thread::spawn(move || {
+            cnt_clone.increment();
+        });
+
+        thread_handles.push(handle);
+    }
+
+    for thread in thread_handles {
+        thread.join().expect("TODO: panic message");
+    }
+
+    println!("Counter is equal: {:?}", cnt.get());
 }
