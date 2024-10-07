90947a7239633c8a8a00eb953348d3184198fd82
MisterZurg <misterzurg@gmail.com>
2024-10-07 23:06:18 +0300

    update l2.5

--- l2/l2-5/src/main.rs
+++ l2/l2-5/src/main.rs
@@ -39,14 +39,54 @@ impl Config {
     }
 }
 
+fn add_before<'a>(before: &usize, curr_idx: usize, lines: &Vec<&'a str>, results: &mut Vec<&'a str>) {
+    if *before == 0 {
+        return;
+    }
+
+    let left_guard = if curr_idx-before > 0 {curr_idx-before} else { 0 };
+    for j in left_guard..curr_idx {
+        results.push(lines[j]);
+    }
+}
+
+fn add_after<'a>(after: &usize, curr_idx: usize, lines: &Vec<&'a str>, results: &mut Vec<&'a str>) {
+    if *after == 0 {
+        return;
+    }
+
+    let right_guard = if curr_idx-after > 0 { curr_idx-after } else { 0 };
+    for j in curr_idx..right_guard {
+        results.push(lines[j]);
+    }
+}
+
 fn search<'a>(cfg: &Config, lines: Vec<&'a str>) -> Vec<&'a str> {
     let mut results = Vec::new();
 
-    let mut before = cfg.before;
-    let mut after= cfg.after;
+    let left_boarder =  if cfg.context > 0 { cfg.context } else { cfg.before };
+    let right_boarder =  if  cfg.context > 0 { cfg.context } else { cfg.after };
 
     for i in 0..lines.len() {
         if lines[i].contains(&cfg.query) {
+            // do something with line
+            add_before(&left_boarder, i, &lines, &mut results);
+
+            results.push(lines[i]);
+
+            add_after(&right_boarder, i, &lines, &mut results)
+        }
+    }
+
+    results
+}
+
+/// cargo run -- -q 'Забрал десять по десять'
+fn search_exact<'a>(cfg: &Config, lines: Vec<&'a str>) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for i in 0..lines.len() {
+        if lines[i].eq(&cfg.query) {
             // do something with line
             results.push(lines[i]);
         }
@@ -68,6 +108,9 @@ fn main() {
         .collect();
 
     let found = search(&cfg, lines);
-
+    // let found = search_exact(&cfg, lines);
+    if cfg.count_lines {
+        println!("{:?}", found.len());
+    }
     println!("{:?}", found);
 }
